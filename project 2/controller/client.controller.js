8d4d8b574cb94135520b90371e1da8c7db709208
emmaedet027-star <emmaedet027@gmail.com>
2026-02-19 08:30:25 +0100

    Update client.controller.js
    second update

--- project 2/controller/client.controller.js
+++ project 2/controller/client.controller.js
@@ -1,43 +1,69 @@
-const Client = require('/model/client');
-const User = require('/model/user');
-// Create client 
-exports.createClient = async (req, res)=>{
-  const{name, email, address, tasktype} = req.body;
-  try{
-     const client = await Client.create(
-     userId: req.userId,
-     name: name,
-     email: email,
-     address: address,
-     tasktype: tasktype,
-     status 
-);
-const user = await User.findOne({_id: req.userId});
-user.usage.clientsCreated++;
-await res.status(200).json({client});  }
-  catch(err){
-  console.log(err);
-} 
+const Client = require('../model/client');
+const User = require('../model/user');
 
-//display all client
-exports.viewClient = async (req, res)=>{
-  let list = [];
-  const clients = await Client.find();
-  for(client of clients):
-  if(client.userId === req.userId){
-    list.push(client);
-    list.forEach(function(l){
-      res.json(${l.name});
-      res.json(list.len(););
+
+// CREATE CLIENT
+exports.createClient = async (req, res) => {
+  try {
+    const { name, email, address, tasktype } = req.body;
+
+    const client = await Client.create({
+      userId: req.userId,
+      name,
+      email,
+      address,
+      tasktype,
+      status: "active"
     });
-  }
-}
 
-//display one client
-exports.viewOneClient = async (req,, res)=>{
-  const client = await Client.findById(req.params.id);
-  if(client.userId !== req.userId){
-    return res.status(400);
+    // update usage
+    const user = await User.findById(req.userId);
+    if (user) {
+      user.usage.clientsCreated++;
+      await user.save();
+    }
+
+    res.status(201).json({ client });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
   }
-  res.json({${client}});
-}
+};
+
+
+
+// VIEW ALL CLIENTS (only logged in user)
+exports.viewClient = async (req, res) => {
+  try {
+    const clients = await Client.find({ userId: req.userId });
+
+    res.json({
+      total: clients.length,
+      clients
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+
+// VIEW ONE CLIENT
+exports.viewOneClient = async (req, res) => {
+  try {
+    const client = await Client.findById(req.params.id);
+
+    if (!client)
+      return res.status(404).json({ message: "Client not found" });
+
+    if (client.userId.toString() !== req.userId)
+      return res.status(403).json({ message: "Not authorized" });
+
+    res.json(client);
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
